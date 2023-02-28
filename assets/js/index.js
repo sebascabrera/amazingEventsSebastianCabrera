7d893a982df888fbd0327584594cb35f4f679951
Sebastian <sebasfedele@gmail.com>
2023-02-28 18:47:20 -0300

    Se agrega precio a 'cards'- Se da inicio a task3

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,20 +4,24 @@
 let cardContainer = document.getElementById('cardid')
 let fragment = document.createDocumentFragment()
 
- for (let i of data.events) {
-      let div = document.createElement(`div`)
-  div.classList.add("card")
-   div.innerHTML = `<img src=${i.image}class="card-img-top" alt="cinema">
+for (let i of data.events) {
+    let div = document.createElement(`div`)
+    div.classList.add("card")
+    div.innerHTML = `<img src=${i.image}class="card-img-top" alt="cinema">
   <div class="card-body">
       <h5 class="card-title" id="card-title">${i.name}</h5>
       <p class="card-text">${i.description}</p>
       <div class="btn-container">
           <a href="./details.html" class="btn btn-outline-info">Details</a>
+        
+             <a href="./details.html" class="btn btn-outline-info">$${i.price}</a>
+           
       </div>
+
   </div>`
     fragment.appendChild(div)
-  }
-  cardContainer.appendChild(fragment)
+}
+cardContainer.appendChild(fragment)
 
 
 
