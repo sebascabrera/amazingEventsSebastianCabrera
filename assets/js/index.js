4bd0495a1ef8933e94960f0c7f858e49e10b09d3
Sebastian <sebasfedele@gmail.com>
2023-02-28 22:12:19 -0300

    Se crea un for en index.js en el que se pasan las 'categories' de data.js como parte de los checkboxes

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,8 +24,28 @@ for (let i of data.events) {
 cardContainer.appendChild(fragment)
 
 
+//////--------------------------------------------------------------------///////////////////////////////////////
 
+// let inputText = document.getElementById('text-input')
+// inputText.addEventListener('keyup', (event)=>{
+//     console.log(inputText.value)
+    
+// })
 
+///////////////---------------------------/////////////////////////
 
+//////////////////traer los check///////////////////
 
+ let checkContainer = document.getElementById(`checkboxes`)
+ let fragment2 = document.createDocumentFragment()
 
+ for (let i of data.events){
+
+     let div = document.createElement(`div`)
+     div.classList.add("form-check")
+     div.innerHTML = `<input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1">
+     <label class="form-check-label" for="flexCheckDefault1">${i.category}</label>`
+
+     fragment2.appendChild(div)
+ }
+ checkContainer.appendChild(fragment2)
