f5a8c3950d822b08546bb2cb5b2260e2e59e55f8
Sebastian <sebasfedele@gmail.com>
2023-03-06 01:04:44 -0300

    funcion de details creada

--- assets/js/details.js
+++ assets/js/details.js
@@ -7,35 +7,35 @@ const params = new URLSearchParams(queryString)
 
 // utilizar el metodo get de serch param para capturar el id -- identificar donde hizo el `click`
 const id = params.get(`id`)
-// vincular id de tarjeta index con id a mostrar de details
-const event = data.events.find(()=>{events.id == id} )
+//const id = new URLSearchParams(location.search).get("id") ==> alternativa
+// vincular id de tarjeta index.js con id de data a mostrar de details
+const event = data.events.find( events => { events.id == id })
 
 
 
-let cardContainer = document.getElementById('card-body-container')
-let fragment = document.createDocumentFragment()
-
-for (let i of data.events){
-    let div = document.createElement(`div`)
-    div.classList.add("card mb-3")
-    div.innerHTML =`<div class="card-detail col no-gutters d-flex ">
+function cardCreator(event) {
+    let container = document.getElementById('details-container-card')
+    let div = document.createElement('div');
+    div.classList.add("card")
+    div.innerHTML = `<div class="card-detail col no-gutters d-flex ">
     <div class="card-detail-img col-md-8 col-sm-12 text-white d-flex justify-content-center">
-      <img src="${i.image}" class="" alt="Imagen de details">
+      <img src="${event.image}" class="" alt="Imagen de details">
     </div>
     <div class="col-md-4 col-sm-12 card-body-container">
       <div class="card-body">
-        <h2 class="card-title fs-1 fw-bold text-center">${i.name}</h2>
+        <h2 class="card-title fs-1 fw-bold text-center">${event.name}</h2>
         <ul>
-          <li>${i.date}</li>
-          <li>${i.description}</li>
-          <li>${i.category}</li>
-          <li>${i.place}</li>
-          <li>${i.capacity}</li>
-          <li>${i.assistence}</li>
-          <li>${i.price}</li>
+          <li>${event.date}</li>
+          <li>${event.description}</li>
+          <li>${event.category}</li>
+          <li>${event.place}</li>
+          <li>${event.capacity}</li>
+          <li>${event.assistence}</li>
+          <li>${event.price}</li>
         </ul>
       </div>
     </div>
   </div>`
-
+  container.appendChild(div)
 }
+cardCreator("details-container-card"); 
