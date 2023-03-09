52be198aba6d85ef824f277fbd130283b0f3c76f
Sebastian <sebasfedele@gmail.com>
2023-03-09 00:29:17 -0300

    se realizo filtro combinado en las card home/pastEvent/upComingEvents

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,127 +1,97 @@
-// let cardTitle = document.getElementById(`cardid`)
-// console.log(cardTitle);
-
-
-
-//la funcion para que pinte las cartas
-//le pasamos el listado de cartas
 let cardContainer = document.getElementById('cardid')
-function showCard(arr , container) {
+function showCard(arr, container) {
+    if ( arr.length > 0){
+        let fragment = document.createDocumentFragment()
+        container.innerHTML = ''
+        for (let i of arr) {
+            let div = document.createElement(`div`)
     
-   
-    let fragment = document.createDocumentFragment()
-    container.innerHTML = ''
-    for (let i of arr) {
-        let div = document.createElement(`div`)
-        
-        div.classList.add("card")
-        div.innerHTML = `<img src=${i.image}class="card-img-top" alt="cinema">
-      <div class="card-body">
-          <h5 class="card-title" id="card-title">${i.name}</h5>
-          <p class="card-text">${i.description}</p>
-          <div class="btn-container">
-              <a href="./details.html?id=${i._id}" class="btn btn-outline-info">Details</a>
-            
-                 <a href="./details.html" class="btn btn-outline-info">$${i.price}</a>
-               
-          </div>
-    
-      </div>`
-        fragment.appendChild(div)
+            div.classList.add("card")
+            div.innerHTML = `<img src=${i.image}class="card-img-top" alt="cinema">
+          <div class="card-body">
+              <h5 class="card-title" id="card-title">${i.name}</h5>
+              <p class="card-text">${i.description}</p>
+              <div class="btn-container">
+                  <a href="./details.html?id=${i._id}" class="btn btn-outline-info">Details</a>
+                                 <a href="./details.html" class="btn btn-outline-info">$${i.price}</a>
+              </div>
+          </div>`
+            fragment.appendChild(div)
+        }
+        container.appendChild(fragment)      
     }
-    container.appendChild(fragment)
+    else{
+        container.innerHTML = ''
+        let div = document.createElement('div')
+               div.innerHTML = ` <div class="card-body">
+          <h5 class="card-title">There is nothing to show you, try again</h5>        
+      </div>`
+      container.appendChild(div) 
+    }
+ 
 }
-
 showCard(data.events, cardContainer)
 
 
 
-//////////////////traer los check///////////////////
+let checkContainer = document.getElementById(`checkboxes`)
+let fragment2 = document.createDocumentFragment()
+let arrsetOfEvent = new Set(data.events.map(event => event.category))
 
- let checkContainer = document.getElementById(`checkboxes`)
- let fragment2 = document.createDocumentFragment()
- let arrsetOfEvent = new Set(data.events.map(event => event.category))
+let arrEvent = [...arrsetOfEvent]
 
- let arrEvent = [...arrsetOfEvent]
+for (let i of arrEvent) {
 
- for (let i of arrEvent) {
-
-     let div = document.createElement(`div`)
-     div.classList.add("form-check")
-     div.innerHTML = `<input class="form-check-input" type="checkbox" value="${i}" id="flexCheckDefault1">
+    let div = document.createElement(`div`)
+    div.classList.add("form-check")
+    div.innerHTML = `<input class="form-check-input" type="checkbox" value="${i}" id="flexCheckDefault1">
       <label class="form-check-label" for="flexCheckDefault1">${i}</label>`
 
-     fragment2.appendChild(div)
- }
- checkContainer.appendChild(fragment2) 
-// // /////////////////////////////////////
+    fragment2.appendChild(div)
+}
+checkContainer.appendChild(fragment2)
+
 function filterArray(arrayString, listCard) {
-
-//return arrayString.length > 0? listCard.filter(events => arrayString.includes(events.category)):listCard;
-
-  
-    if (arrayString.length == 0){
-        
+    //return arrayString.length > 0? listCard.filter(events => arrayString.includes(events.category)):listCard;
+    if (arrayString.length == 0) {
         return listCard
-    }else {
-        
-      return listCard.filter(events => arrayString.includes(events.category))
-      
+    } else {
+        return listCard.filter(events => arrayString.includes(events.category))
     }
- }
-
-let checkedInputs = []
-
-let checkboxes = document.querySelectorAll('input[type=checkbox]')
-
-
-checkboxes.forEach(checkbox => checkbox.addEventListener('change', selected))
-
-//tengo el array de checkboxes capturados
-
-function selected() {
-    
-    checkedInputs = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(input => input.value)
-console.log(checkedInputs);
-    let vble = filterArray(checkedInputs, data.events) // agregar a la funcion de filtro cruzado
-  
-   showCard(vble, cardContainer)
 }
 
+let checkedInputs = []
+let checkboxes = document.querySelectorAll('input[type=checkbox]')
+checkboxes.forEach(checkbox => checkbox.addEventListener('change', selected))
 
 
+function selected() {
 
-
-
-
-
-
-
-// //////////////// capturar 
-// // let inputText = document.getElementById('text-input')
-// // inputText.addEventListener('keyup', (event)=>{
-// //     console.log(inputText.value)
-// // let nombre = lista de Input.fine(nombre =>{
-// //     nombre.serch(e.target.value)
-// //     retur nombre.toLowerCase().serch(e.target.value.toLowerCase().trim() ) !=1
-// // } )
-
-// // })
+    checkedInputs = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(input => input.value)
+    console.log(checkedInputs);
+   
+    combineFilters(data.events)
+}
 
 let inputText = document.getElementById('text-input')
 let sercher = ""
 inputText.addEventListener('keyup', (e) => {
-    //e.preventDefault(); 
+ 
     sercher = e.target.value;
-    console.log(sercher); // aca va el llamado de funcion combinada
-    // readerSercher(sercher, data.events)
-    // console.log(readerSercher(sercher, data.events));
-    // showCard(readerSercher(sercher, data.events),cardContainer) // filtra solo con el serch
+    console.log(sercher);   
+    combineFilters(data.events)
 })
 
 function readerSercher(textInput, listCard) {
     if (textInput == "") return listCard
-    return listCard.filter(card => card.name.toLowerCase().includes(textInput.toLowerCase().trim()))       
-    
+    return listCard.filter(card => card.name.toLowerCase().includes(textInput.toLowerCase().trim()))
+
+}
+
+function combineFilters(array) {
+
+    let cardFilterArray = filterArray(checkedInputs, array);
+    let stringFilter = readerSercher(sercher, cardFilterArray);
+    showCard(stringFilter, cardContainer)
 }
 
--- assets/js/upcomingEvents.js
+++ assets/js/upcomingEvents.js
@@ -1,25 +1,98 @@
- //let cardContainer = document.getElementById('cardpast')
- let currentDt = parseInt(data.currentDate)
- console.log(typeof (currentDt));
- 
- let cardFutContainer = document.getElementById('futurecard')
- let fragment = document.createDocumentFragment()
- 
-  for (let i of data.events) {
-     let iDate = parseInt(i.date)
-     if( currentDt <= iDate ){
-         let div = document.createElement(`div`)
-         div.classList.add("card")
-          div.innerHTML = `<img src=${i.image}class="card-img-top" alt="cinema">
-         <div class="card-body">
-             <h5 class="card-title" id="card-title">${i.name}</h5>
-             <p class="card-text">${i.description}</p>
-             <div class="btn-container">
-                 <a href="./details.html" class="btn btn-outline-info">Details</a>
-             </div>
-         </div>`
-           fragment.appendChild(div)
-     }
-   
-   }
-   cardFutContainer.appendChild(fragment)
+//let cardContainer = document.getElementById('cardpast')
+let currentDt = parseInt(data.currentDate)
+
+
+let cardContainer = document.getElementById('futurecard')
+function showCard(arr, container) {
+    if (arr.length > 0) {
+        let fragment = document.createDocumentFragment()
+        container.innerHTML = ''
+        for (let i of arr) {
+            let iDate = parseInt(i.date)
+            if( currentDt <= iDate ){
+                let div = document.createElement(`div`)
+                div.classList.add("card")
+                 div.innerHTML = `<img src=${i.image}class="card-img-top" alt="cinema">
+                <div class="card-body">
+                    <h5 class="card-title" id="card-title">${i.name}</h5>
+                    <p class="card-text">${i.description}</p>
+                    <div class="btn-container">
+                        <a href="./details.html" class="btn btn-outline-info">Details</a>
+                    </div>
+                </div>`
+                  fragment.appendChild(div)
+            }
+        }
+        cardContainer.appendChild(fragment)
+    }
+    else{
+        container.innerHTML = ''
+        let div = document.createElement('div')
+               div.innerHTML = ` <div class="card-body">
+          <h5 class="card-title">There is nothing to show you, try again</h5>        
+      </div>`
+      container.appendChild(div)  
+    }
+}
+showCard(data.events, cardContainer)
+
+let checkContainer = document.getElementById(`checkboxes`)
+let fragment2 = document.createDocumentFragment()
+let arrsetOfEvent = new Set(data.events.map(event => event.category))
+
+let arrEvent = [...arrsetOfEvent]
+
+for (let i of arrEvent) {
+
+    let div = document.createElement(`div`)
+    div.classList.add("form-check")
+    div.innerHTML = `<input class="form-check-input" type="checkbox" value="${i}" id="flexCheckDefault1">
+      <label class="form-check-label" for="flexCheckDefault1">${i}</label>`
+
+    fragment2.appendChild(div)
+}
+checkContainer.appendChild(fragment2)
+
+function filterArray(arrayString, listCard) {
+    //return arrayString.length > 0? listCard.filter(events => arrayString.includes(events.category)):listCard;
+    if (arrayString.length == 0) {
+        return listCard
+    } else {
+        return listCard.filter(events => arrayString.includes(events.category))
+    }
+}
+
+let checkedInputs = []
+let checkboxes = document.querySelectorAll('input[type=checkbox]')
+checkboxes.forEach(checkbox => checkbox.addEventListener('change', selected))
+
+
+function selected() {
+
+    checkedInputs = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(input => input.value)
+    console.log(checkedInputs);
+
+    combineFilters(data.events)
+}
+
+let inputText = document.getElementById('text-input')
+let sercher = ""
+inputText.addEventListener('keyup', (e) => {
+
+    sercher = e.target.value;
+    console.log(sercher);
+    combineFilters(data.events)
+})
+
+function readerSercher(textInput, listCard) {
+    if (textInput == "") return listCard
+    return listCard.filter(card => card.name.toLowerCase().includes(textInput.toLowerCase().trim()))
+
+}
+
+function combineFilters(array) {
+
+    let cardFilterArray = filterArray(checkedInputs, array);
+    let stringFilter = readerSercher(sercher, cardFilterArray);
+    showCard(stringFilter, cardContainer)
+}
