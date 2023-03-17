646983fdacb26e822a6ae0cdaea7b4bd6498d942
Sebastian <sebasfedele@gmail.com>
2023-03-17 00:03:13 -0300

    primer tabla creada

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,37 +1,47 @@
 const queryString = location.search
-
 const params = new URLSearchParams(queryString)
+const ids = params.get("id")
 
+const url = "https://mindhub-xj03.onrender.com/api/amazing"
+async function obtaindata(urlDirection) {
+  console.log(ids);
+  try {
+    let response = await fetch(urlDirection)
+    const data = await response.json()
+    const cardObject = data.events.find(event => event._id == ids)
+    cardCreator(cardObject)
+   
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+//const cardObject = data.events.find(event => event._id == id)
+obtaindata(url)
 
-const id = params.get("id")
-
-
-const cardObject = data.events.find(event => event._id == id)
-
-console.log(Object.keys(cardObject));
-function cardCreator() {
+function cardCreator(param) {
   let container = document.getElementById('details-container-card')
   let div = document.createElement('div');
   div.classList.add("card3");
   div.innerHTML = `<div class="card-detail col no-gutters d-flex ">
     <div class="card-detail-img col-md-8 col-sm-12 text-white d-flex justify-content-center">
-      <img src="${cardObject.image}" class="card-img-top detail-img" alt="Imagen de details">
+      <img src="${param.image}" class="card-img-top detail-img" alt="Imagen de details">
     </div>
     <div class="col-md-10 col-sm-12 card-body-container">
       <div class="card-body">
-        <h2 class="card-title fs-1 fw-bold text-center">${cardObject.name}</h2>
+        <h2 class="card-title fs-1 fw-bold text-center">${param.name}</h2>
         <ul>
-          <li><p>${Object.keys(cardObject)[3].toUpperCase()} </p> ${cardObject.date}</li>
-          <li id="li-description"><p>${Object.keys(cardObject)[4].toUpperCase()} </p> ${cardObject.description}</li>
-          <li><p>${Object.keys(cardObject)[5].toUpperCase()} </p> ${cardObject.category}</li>
-          <li><p>${Object.keys(cardObject)[6].toUpperCase()} </p>${cardObject.place}</li>
-          <li><p>${Object.keys(cardObject)[7].toUpperCase()} </p>${cardObject.capacity}</li>
-          <li><p>${Object.keys(cardObject)[8].toUpperCase()} </p>${cardObject.assistance ? cardObject.assistance : cardObject.estimate}</li>
-          <li><p>${Object.keys(cardObject)[9].toUpperCase()} </p>${cardObject.price}</li>
+          <li><p>${Object.keys(param)[3].toUpperCase()} </p> ${param.date}</li>
+          <li id="li-description"><p>${Object.keys(param)[4].toUpperCase()} </p> ${param.description}</li>
+          <li><p>${Object.keys(param)[5].toUpperCase()} </p> ${param.category}</li>
+          <li><p>${Object.keys(param)[6].toUpperCase()} </p>${param.place}</li>
+          <li><p>${Object.keys(param)[7].toUpperCase()} </p>${param.capacity}</li>
+          <li><p>${Object.keys(param)[8].toUpperCase()} </p>${param.assistance ? param.assistance : param.estimate}</li>
+          <li><p>${Object.keys(param)[9].toUpperCase()} </p>${param.price}</li>
         </ul>
       </div>
     </div>
   </div>`
   container.appendChild(div)
 }
-cardCreator(); 
+//cardCreator(); 
--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,12 +1,40 @@
-console.log("hola");
+const url = "https://mindhub-xj03.onrender.com/api/amazing"
+async function obtaindata(urlparam) {
 
+    try {
+        let response = await fetch(urlparam)
+        const data = await response.json()
 
-fetch("https://mindhub-xj03.onrender.com/api/amazing")
-//.then(response => console.log(response))
-.then(response => response.json()) // aca estoy obteniendo el archivo json con el que voy a trabajar
-.then(data =>(data.events))
+        let arrEvent = data.events
+        let tblContainer = document.getElementById('firstTable')
+        createTable(arrEvent, tblContainer)
 
-// if (response.status === 400){
-//     let datos = await response.json()
-//     console.log(datos.loquesea);
-// }
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+obtaindata(url)
+
+function createTable(arr, container) {
+
+    let bestCapacity = arr.sort((a, b) => b.capacity - a.capacity)
+    let bestAttendance = arr.filter(elemento => elemento.assistance).reduce((evento1, evento2) => {
+        if ((evento1.assistance / evento1.capacity) > (evento2.assistance / evento2.capacity)) return evento1
+        return evento2
+    })
+    console.log(bestAttendance);
+    let lowerAttendance = arr.filter(elemento => elemento.assistance).reduce((evento1, evento2) => {
+        if ((evento1.assistance / evento1.capacity) < (evento2.assistance / evento2.capacity)) return evento1
+        return evento2
+    })
+
+    let tr = document.createElement('tr')
+    tr.innerHTML = ''
+    tr.innerHTML = `<td>${bestAttendance.name} : ${bestAttendance.assistance}</td>
+            <td>${lowerAttendance.name} : ${lowerAttendance.assistance}</td>
+            <td>${bestCapacity[0].name} : ${bestCapacity[0].capacity}</td>`
+
+    container.appendChild(tr)
+
+}
